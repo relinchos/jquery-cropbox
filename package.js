3510e3f29088e1ca7aa4cdf3dd54678f2e498066
santiago <contacto.quinteros>
2015-12-07 12:05:58 -0300

    Initial commit

---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: "relinchos:jquery-cropbox",
+  summary: "jquery-crobox ",
+  version: "0.2.2",
+  documentation:null
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
+  api.use('jquery', 'client');
+  api.imply('jquery', 'client');
+  api.addFiles([
+    'jquery-cropbox/jquery.cropbox.js',
+  ], 'client'
+  );
+});
