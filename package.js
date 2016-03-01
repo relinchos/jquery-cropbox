71056ba7f4960fa154160cee224839b07bdf7fd6
Santiago Quinteros <contacto.quinteros@gmail.com>
2016-03-01 16:10:35 -0300

    update version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "relinchos:jquery-cropbox",
   summary: "jquery-crobox",
-  version: "0.2.7",
+  version: "0.2.8",
   git: 'https://github.com/relinchos/jquery-cropbox',
   documentation: 'README.md'
 });
