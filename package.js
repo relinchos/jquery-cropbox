9d6ec90deb523dcb88be533f94305a15dee7f87b
leandropio <leandro.coding@gmail.com>
2016-03-03 05:21:31 -0300

    Increase version nmber

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "relinchos:jquery-cropbox",
   summary: "jquery-crobox",
-  version: "0.3.11",
+  version: "0.3.12",
   git: 'https://github.com/relinchos/jquery-cropbox',
   documentation: 'README.md'
 });
